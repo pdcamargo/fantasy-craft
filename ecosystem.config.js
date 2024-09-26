337b98997c30c2a0d16652ba07c7fc2dc13662e2
Patrick Dias <pdcamargo@hotmail.com>
2024-09-26 00:59:24 -0300

    Fix pm2 env variable to remove unsafe data

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,9 +10,6 @@ module.exports = {
         NODE_ENV: "production",
         API_URL: "https://api.fantasycraft.io",
         NEXTAUTH_URL: "https://fantasycraft.io",
-        NEXTAUTH_SECRET: "secret",
-        DATABASE_URL:
-          "postgresql://postgres:JJM2h33@@89.116.51.219:5432/fantasycraft",
         PRISMA_SCHEMA_DISABLE_ADVISORY_LOCK: "true",
       },
     },
