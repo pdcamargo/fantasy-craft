584785fb375a55f9dd12ec69b3e5ca6a3bea458b
Patrick Dias <pdcamargo@hotmail.com>
2024-09-26 00:55:32 -0300

    Fix pm2 env variable

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,6 +11,9 @@ module.exports = {
         API_URL: "https://api.fantasycraft.io",
         NEXTAUTH_URL: "https://fantasycraft.io",
         NEXTAUTH_SECRET: "secret",
+        DATABASE_URL:
+          "postgresql://postgres:JJM2h33@@89.116.51.219:5432/fantasycraft",
+        PRISMA_SCHEMA_DISABLE_ADVISORY_LOCK: "true",
       },
     },
   ],
